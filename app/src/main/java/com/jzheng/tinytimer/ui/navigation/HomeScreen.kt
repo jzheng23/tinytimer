9e9161f0a2b41d02769e891fda245f5bd582c819
Jian Zheng <119757037+jzheng23@users.noreply.github.com>
2025-04-18 13:01:33 -0400

    updated

--- app/src/main/java/com/jzheng/tinytimer/ui/navigation/HomeScreen.kt
+++ app/src/main/java/com/jzheng/tinytimer/ui/navigation/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.jzheng.tinytimer.ui.navigation
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
